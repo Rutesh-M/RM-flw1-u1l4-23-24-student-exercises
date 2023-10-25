5a0d00bf43e2226a5d5dbcaa656c59a4d33c7827
Rutesh-M <124737800+Rutesh-M@users.noreply.github.com>
2023-10-25 22:27:20 +0000

    I did the work

--- conditionals.js
+++ conditionals.js
@@ -3,17 +3,22 @@
 // recommends an ice cream flavor based on the current temperature in Fahrenheit.
 
 // 1. Create a variable named temperature any value between 32 and 68.
-
+let temperature= 20;
 
 // 2. Create a variable named recommendedFlavor. Do not assign a value.
-
+let recommendedFlavour;
 
 // 3. Implement the recommendation logic
 // - If the temperature is less than 50, recommend "Hot Chocolate Fudge".
 // - If the temperature is between 50 and 68 (inclusive), recommend "Caramel Crunch".
 // - If the temperature is greater than 68, recommend "Lemon Sorbet".
-
+if(temperature<50){
+  recommendedFlavour="Hot Chocolate Fudge";
+}else if(temperature<=68){
+  recommendedFlavour="Caramel Crunch"
+}else{
+   recommendedFlavour="Lemon Sorbet"
+}
 
 // 4. Console log the recommended flavor.
-
-
+console.log(recommendedFlavour)
